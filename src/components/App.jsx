3d401c81d58ef1ad35a924c6da0bccc09abeabb3
Gautier <wapfal0@gmail.com>
2023-10-04 14:13:22 +0200

    Added PokemonCard component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,64 +1,21 @@
 // import { useState } from 'react'
 import '../styles/normalize.css'
 import '../styles/style.css'
-import data from '../data/data.json'
 import logo from '../assets/logo.png'
-
-function getColorHexa(type) {
-  let color;
-    switch(type) {
-      case 'Eau':
-        color = 'blue';
-        break;
-
-      case 'Plante':
-        color = 'green';
-        break;
-
-      case 'Feu':
-        color = 'orange';
-        break;
-
-      default:
-        color = 'grey';
-        break;  
-  }
-  return color
-}
+import data from '../data/data.json'
+import PokemonCard from './PokemonCard'
 
 function App() {
-  const pokemonList =
-    <main>
-      {data.map((pokemon) => (
-        <article key={pokemon.pokedexId}  style={{
-          borderColor: getColorHexa(pokemon.apiTypes[0].name),
-          backgroundColor: getColorHexa(pokemon.apiTypes[0].name),
-        }}>
-          <figure>
-          <picture>
-              <img src={pokemon.image} alt="Image ${pokemon.name}" />
-          </picture>
-          <figcaption>
-              <span className="types">{pokemon.apiTypes[0].name}</span>
-              <h2>{pokemon.name}</h2>
-              <ol>
-              <li>Points de vie : {pokemon.stats.HP}</li>
-              <li>Attaque : {pokemon.stats.attack}</li>
-              <li>Défense : {pokemon.stats.defense}</li>
-              <li>Attaque spécial : {pokemon.stats.special_attack}</li>
-              <li>Vitesse : {pokemon.stats.speed}</li>
-              </ol>
-          </figcaption>
-          </figure>
-      </article>))}
-    </main>
-
   return (
     <>
       <h1>
         <img src={logo} alt='Logo Pokémon' width="100%" height="100%"/>
       </h1>
-      {pokemonList}
+      <main>
+      {data.map((elem) => (
+        <PokemonCard key={elem.pokedexId} pokemon={elem}/>
+      ))}
+      </main>
     </>
   )
 }
