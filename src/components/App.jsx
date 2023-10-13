8eb49e684b2fd11e8f081231e3369b115bf91ffb
Gautier <wapfal0@gmail.com>
2023-10-13 09:33:25 +0200

    Implemented the API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,8 +8,23 @@ import Sidebar from './Sidebar/Sidebar'
 
 function App() {
   const [pokedex, setPokedex] = useState([])
-  const helloList = data.map((poke, index) => (
-    <CardPokemon data={poke} key={index} pokedex={pokedex} setPokedex={setPokedex}/>
+  const [loading, setLoading] = useState(false)
+  const [dataPokemon, setDataPokemon] = useState([])
+  useEffect(() => {
+    fetch('https://pokebuildapi.fr/api/v1/pokemon/generation/1')
+       .then((response) => response.json())
+       .then((dataPokemon) => {
+          console.log(dataPokemon);
+          setDataPokemon(dataPokemon);
+          setLoading(true)
+       })
+       .catch((err) => {
+          console.log(err.message);
+       });
+  }, []);
+
+  const helloList = dataPokemon.map((poke, index) => (
+      <CardPokemon data={poke} key={index} pokedex={pokedex} setPokedex={setPokedex}/>
   ));
   useEffect(() => {
     console.log(`Le Pokedex possède ${pokedex.length} pokémons`)
@@ -22,7 +37,12 @@ function App() {
       </h1>
       <div className="lmj-layout-inner">
         <Sidebar pokedex={pokedex}/>
-        <main>{helloList}</main>
+        <main>
+          { loading ? (
+            [helloList]
+          ) : (
+            <p>Chargement</p>
+          )}</main>
       </div>
     </>
   );
